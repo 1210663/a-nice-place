6545fdd782f776cb2cc5db51a45f84ef443c3f73
1210663 <noahbradford63@gmail.comdoma1nex>
2024-03-04 17:44:42 -0500

    evedam update

--- Assets/Adem.cs
+++ Assets/Adem.cs
@@ -7,7 +7,9 @@ public class shotocharacter : MonoBehaviour
     public float jumpForce = 10f;
     // Speed of movement
     public float moveSpeed = 5f;
-        
+    // Number of times to perform the custom action
+    public int actionCount = 3;
+
     //Reference to the Rigidbody
     private Rigidbody2D rb;
 
@@ -16,18 +18,24 @@ public class shotocharacter : MonoBehaviour
     // Whether the character is currently blocking
     private bool isBlocking;
 
-    void Start()
+    // List of custom actions
+    private List<string> customActions = new List<string> { "Action1", "Action2", "Action3" };
+
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-  
-   // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
         // Horizontal movement
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
-
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
         // Jumping
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
@@ -44,6 +52,9 @@ public class shotocharacter : MonoBehaviour
         {
             isBlocking = false;
         }
+
+        // Perform custom action
+        PerformCustomAction();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -51,6 +62,24 @@ public class shotocharacter : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+        }
+    }
+
+    // Custom function to perform an action multiple times
+    private void PerformCustomAction()
+    {
+        // For loop to perform the action multiple times
+        for (int i = 0; i < actionCount; i++)
+        {
+            Debug.Log("Performing action " + (i + 1));
+        }
+
+        // While loop to perform the action while a condition is met
+        int index = 0;
+        while (index < customActions.Count)
+        {
+            Debug.Log("Performing custom action: " + customActions[index]);
+            index++;
         }
     }
 }
